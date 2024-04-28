687d3b0b0d1fde14ad16d85358c31edf5e41fd51
Oke.A <Okea.dev@gmail.com>
2024-04-28 13:40:02 +0100

    ..All test has been completely successfully..

--- test/CounterWithTimerTest.js
+++ test/CounterWithTimerTest.js
@@ -22,15 +22,61 @@ describe("CounterWithTimerModule", function () {
   }
 
   describe("Deployment", function () {
-    it("Initial count is set to 0", async function () {
+    it("should have an initial count of 0", async function () {
       const { counterWithTimer, owner } = await loadFixture(deployCounterWithTimer);
       expect(await counterWithTimer.count()).to.equal(0);
     });
 
-    it("Increment the count", async function () {
-      const { counterWithTimer, owner } = await loadFixture(deployCounterWithTimer);
-      expect(await counterWithTimer.increment()).to.be.reverted;
+    it("Should increment the count", async function () {
+      const { counterWithTimer} = await loadFixture(deployCounterWithTimer);
+      const initialCount = await counterWithTimer.count();
+      await counterWithTimer.increment();
+      expect(await counterWithTimer.count()).to.equal((initialCount + 1n));
+      
     });
+
+    it("should decrement the count", async function () {
+      const { counterWithTimer} = await loadFixture(deployCounterWithTimer);
+      await counterWithTimer.increment();
+      const initialCount = await counterWithTimer.count();
+      await counterWithTimer.decrement();
+      expect(await counterWithTimer.count()).to.equal((initialCount - 1n));
+      
+    });
+
+    it("Should should set the timer", async function () {
+      const { counterWithTimer} = await loadFixture(deployCounterWithTimer);
+      const delay = 60; //1 minute
+      await counterWithTimer.setTimer(delay);
+      expect(await counterWithTimer.getTimeRemaining()).to.be.closeTo(delay, 2);
+      
+    });
+
+    it("Should block increment and decrement when timer is running", async function () {
+      const { counterWithTimer} = await loadFixture(deployCounterWithTimer);
+      const delay = 60;
+      await counterWithTimer.setTimer(delay);
+      await expect( counterWithTimer.increment()).to.be.revertedWith("Timer is still running");
+      await expect( counterWithTimer.decrement()).to.be.revertedWith("Timer is still running");
+      
+    });
+
+    it("Should allow increment after timer expires", async function () {
+      const { counterWithTimer} = await loadFixture(deployCounterWithTimer);
+      const delay = 2;
+      await counterWithTimer.setTimer(delay);
+      await ethers.provider.send("evm_increaseTime", [delay + 1]);
+      await ethers.provider.send("evm_mine");
+      await counterWithTimer.increment();
+      expect(await counterWithTimer.getCount()).to.equal(1);
+
+    })
+
+
+
+    
+
+
     
     
    
